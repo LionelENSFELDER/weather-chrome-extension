2c1238e7317d620d5374ec18fe84cba206817b79
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2020-04-20 14:20:47 +0200

    add input and get its value

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 console.log("start");
 
-var data = null;
+function getWeather(input){
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = false;
+    let data = null;
+    let city = input;
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=f61845cc2a438aef2ac2289b15f69307";
+    console.log("url= "+url);
+    let xhr = new XMLHttpRequest();
 
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-        let weatherObj = JSON.parse(this.responseText);
-            console.log('Country: ' + weatherObj.name);
-            console.log('Current temp: ' + weatherObj.main.temp + '°');
-            return weatherObj;
-	}
-});
+    xhr.withCredentials = false;
+    
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+            let weatherObj = JSON.parse(this.responseText);
+                console.log('Country: ' + weatherObj.name);
+                console.log('Current temp: ' + weatherObj.main.temp + '°');
+                return weatherObj;
+        }
+    });
+    
+    xhr.open("GET", url);
+    
+    xhr.send(data);
+}
 
-xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=f61845cc2a438aef2ac2289b15f69307");
-
-xhr.send(data);
+function getLocation() {
+    let input = document.getElementById("locationInput").value;
+    console.log(input);
+    getWeather(input);
+}
